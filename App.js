4a54e231f2f1f55370c2483a5adddb306a14815a
MarcosIraola <iraolamarcos@gmail.com>
2022-09-12 19:44:41 -0300

    dependencias

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Pokedex</Text>
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <View style={styles.container}>
+                <Text>Pokedex</Text>
+            </View>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
