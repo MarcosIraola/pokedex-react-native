97229e97753354dacd26bc611ec1a801e4e21d30
MarcosIraola <iraolamarcos@gmail.com>
2022-09-13 00:37:48 -0300

    pokemon card, bg image

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,9 +1,13 @@
-import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableWithoutFeedback, ImageBackground } from 'react-native'
 import React from 'react'
+import getColorByPokemonType from '../../utils/colors';
 
 export default function PokemonCard(props) {
 
     const { pokemon } = props;
+
+    const pokemonColor = getColorByPokemonType(pokemon.type);
+    const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
 
     const goToPokemon = () => {
         console.log(`Vamos a: ${pokemon.name}`)
@@ -13,10 +17,9 @@ export default function PokemonCard(props) {
         <TouchableWithoutFeedback onPress={goToPokemon}>
             <View style={styles.card}>
                 <View style={styles.spacing}>
-                    <View style={styles.bgStyles}>
-                        {/* <Text style={styles.order}>#{pokemon.order}</Text> */}
+                    <View style={bgStyles}>
+                        <ImageBackground style={styles.bgImage} source={require('../assets/background-test.png')}/>
                         <Text style={styles.number}>#{`${pokemon.order}`.padStart(3,0)}</Text>
-
                         <Text style={styles.name}>{pokemon.name}</Text>
                         <Image source={{uri: pokemon.imagen}} style={styles.image}/>
                     </View>
@@ -36,12 +39,14 @@ const styles = StyleSheet.create({
         padding: 5
     },
     bgStyles: {
-        backgroundColor: "grey",
+        flex: 1,
+        borderRadius: 15,
+        padding: 10
     },
     image: {
         position:"absolute",
         bottom: 10,
-        right: 0,
+        right: 10,
         width: 90,
         height: 90
     },
@@ -49,16 +54,25 @@ const styles = StyleSheet.create({
         color: "#fff",
         fontWeight: "bold",
         fontSize: 15,
-        padding: 8,
+        padding: 0,
         textTransform: 'capitalize'
     },
     number: {
         position: 'absolute',
         right: 10,
-        top: 10,
-        fontSize: 12,
+        top: 8,
+        fontSize: 14,
         paddingLeft: 8,
         paddingTop: 5,
         color: '#fff'
+    },
+    bgImage: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        opacity: 0.1,
+        resizeMode: 'contain'
     }
 })
