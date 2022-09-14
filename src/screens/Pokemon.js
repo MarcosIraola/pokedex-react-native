c3f7e668f859a57e704f7b92327b69011bd0f991
MarcosIraola <iraolamarcos@gmail.com>
2022-09-13 23:41:27 -0300

    navigation bar top, cambio iconos

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -4,13 +4,34 @@ import { getPokemonById } from '../api/pokemon';
 import Header from '../components/Pokemon/Header';
 import Type from '../components/Pokemon/Type';
 import Stats from '../components/Pokemon/Stats';
-
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 export default function Pokemon(props) {
 
     const { navigation, route: {params} } = props;
 
     const [pokemon, setPokemon] = useState()
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => 
+            <Icon 
+                name="heart" 
+                color="#fff" 
+                size={20} 
+                style={{ marginRight: 20, marginTop: 10}}
+                onPress={() => console.log('Favorite')}
+            />,
+            headerLeft: () => 
+                <Icon 
+                    name="arrow-left" 
+                    color="#fff" 
+                    size={20} 
+                    style={{ marginLeft: 20, marginTop: 10}}
+                    onPress={navigation.goBack}
+                />
+        })
+    }, [navigation, params])
 
     useEffect (() => {
         (async () => {
