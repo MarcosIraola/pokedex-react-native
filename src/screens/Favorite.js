21a415f20ade17a444fcbae94d39371c7c4e5eb8
MarcosIraola <iraolamarcos@gmail.com>
2022-09-15 23:07:59 -0300

    navigation ok from favorites

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,21 +1,47 @@
-import { SafeAreaView, Text, Button } from 'react-native'
-import React, { useState } from 'react'
+import { Text } from 'react-native'
+import React, { useState, useCallback } from 'react'
+import { useFocusEffect } from "@react-navigation/native"
 import { getPokemonFavorites } from '../api/favorite'
+import { getPokemonById } from '../api/pokemon'
+import useAuth from '../hooks/useAuth';
+import PokemonList from '../components/PokemonList';
 
 export default function Favorite() {
 
-    const [favorites, setFavorites] = useState(null)
+    const [pokemons, setPokemons] = useState([]);
+    const { auth } = useAuth();
 
+    useFocusEffect(
+        useCallback(() => {
+            if(auth) {
+                (async () => {
+                    const response = await getPokemonFavorites();
+                    const pokemonsArray = []
+    
+                    for await (const id of response) {
+                        const pokemonDetails = await getPokemonById(id)
+                        pokemonsArray.push({
+                            id: pokemonDetails.id,
+                            name: pokemonDetails.name,
+                            type: pokemonDetails.types[0].type.name,
+                            order: pokemonDetails.order,
+                            imagen: pokemonDetails.sprites.other['official-artwork'].front_default
+                        })
+                    }
+                    setPokemons(pokemonsArray)
+                })()
+            }
+        }, [auth])
+    )
 
-    const checkFavorites = async() => {
-        const response = await getPokemonFavorites()
-        console.log(response);
-    }
+    
 
     return (
-        <SafeAreaView>
-            <Text>Favorite</Text>
-            <Button title='get favorites' onPress={checkFavorites}/>
-        </SafeAreaView>
+        !auth ? (
+            <Text>Usuario no loggeado</Text>
+        ) : (
+            // <Text>Lista de pokemons favoritos</Text>
+            <PokemonList pokemons={pokemons}/>
+        )
     )
 }
